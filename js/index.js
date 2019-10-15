756810bbb997feeebde2bdb90f4e6609040495b7
Teresa Gorbea <teresa.gorbeah@gmail.com>
2019-10-14 19:13:49 -0500

    menushow

---
+++ js/index.js
@@ -0,0 +1,15 @@
+jQuery ('document').ready(function($){
+    var menuBtn = $('.menu-icon');
+    var menu = $('.navigation ul');
+    
+    menuBtn.click(function(){
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }else{
+            menu.addClass('show');
+        }
+
+    });
+
+
+});
